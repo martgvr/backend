08c55257a0971846518ebf2a18e99543fb1b9298
Martin Guevara <martin_91@live.com.ar>
2022-09-19 12:01:50 -0300

    Desafio: Motores de plantillas

--- desafio_5/pug/src/server.js
+++ desafio_5/pug/src/server.js
@@ -10,10 +10,6 @@ app.use('/', usersRoutes);
 app.set('views', './src/views')
 app.set('view engine', 'pug')
 
-// app.get('/', (req, res) => {
-//     res.render('index', { form: false })
-// })
-
 const PORT = process.env.PORT || 8082;
 const server = app.listen(PORT, (req, res) => console.log(`Escuchando el puerto ${PORT}`))
 server.on('error', error => console.log(`Error: ${error}`));
