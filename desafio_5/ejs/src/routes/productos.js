cb4839000712c07ffa4930a1ecdc266848ac217f
Martin Guevara <martin_91@live.com.ar>
2022-09-19 11:23:57 -0300

    Desafio: Motores de plantillas

--- desafio_5/ejs/src/routes/productos.js
+++ desafio_5/ejs/src/routes/productos.js
@@ -15,5 +15,24 @@ async function getAll() {
 router.get('/', (req, res) => res.render('pages/index', { form: true }));
 router.get('/productos', (req, res) => getAll().then(data => res.render('pages/index', { form: false, data })));
 
+router.post('/productos', (req, res) => {
+    const product = {
+        title: req.body.title,
+        price: Number(req.body.price),
+        thumbnail: req.body.thumbnail
+    }
+    getAll()
+        .then((data) => {
+            data.push({ ...product, id: data.length + 1 });
+            fs.promises.writeFile('./src/productos.txt', JSON.stringify(data));
+            res.redirect('/');
+        })
+        .catch((e) => {
+            product.id = 1;
+            fs.writeFileSync('./src/productos.txt', `[${JSON.stringify(product)}]`);
+            res.redirect('/');
+        })
+});
+
 // EXPORT
 module.exports = router;
--- desafio_5/ejs/src/server.js
+++ desafio_5/ejs/src/server.js
@@ -10,25 +10,6 @@ app.use('/', usersRoutes);
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
-// const usuarios = [
-//     {
-//         nombre: 'Alan',
-//         apellido: 'Gomez'
-//     },
-//     {
-//         nombre: 'Juan',
-//         apellido: 'Perez'
-//     },
-//     {
-//         nombre: 'Andres',
-//         apellido: 'Gutierrez'
-//     }
-// ]
-
-// app.get('/', (req, res) => {
-//     res.render('pages/index', { usuarios, form: true })
-// })
-
 const PORT = process.env.PORT || 8082;
 const server = app.listen(PORT, (req, res) => console.log(`Escuchando el puerto ${PORT}`))
 server.on('error', error => console.log(`Error: ${error}`));
