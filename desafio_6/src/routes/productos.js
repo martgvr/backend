192e9cb437b4f714f333f4e73bc07a4c04d37125
Martin Guevara <martin_91@live.com.ar>
2022-09-24 21:21:37 -0300

    Desafio: Websockets

---
+++ desafio_6/src/routes/productos.js
@@ -0,0 +1,40 @@
+const { Router } = require('express');
+const router = Router();
+const fs = require('fs');   
+
+async function getAll() {
+    try {
+        const response = await fs.promises.readFile('./src/productos.txt', 'utf-8');
+        return JSON.parse(response);
+    } catch (e) {
+        return { error: true } 
+    }
+}
+
+// ENDPOINTS
+router.get('/', (req, res) => res.render('index', { table: false }));
+
+router.get('/productos', (req, res) => getAll().then(data => res.render('index', { table: true, data })));
+
+router.post('/productos', (req, res) => {
+    const product = {
+        title: req.body.title,
+        price: Number(req.body.price),
+        thumbnail: req.body.thumbnail
+    }
+    getAll()
+        .then((data) => {
+            data.push({ ...product, id: data.length + 1 });
+            fs.promises.writeFile('./src/productos.txt', JSON.stringify(data));
+            res.redirect('/');
+        })
+        .catch((e) => {
+            product.id = 1;
+            fs.writeFileSync('./src/productos.txt', `[${JSON.stringify(product)}]`);
+            res.redirect('/');
+        })
+});
+
+
+// EXPORT
+module.exports = router;
---
+++ desafio_6/src/server.js
@@ -0,0 +1,31 @@
+const usersRoutes = require('./routes/productos.js');
+const hbs = require('express-handlebars');
+const express = require('express');
+const app = express();
+
+const http = require("http");
+const { Server: SocketServer } = require("socket.io");
+const httpServer = http.createServer(app);
+const socketServer = new SocketServer(httpServer);
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/', usersRoutes);
+
+app.set('views', './src/views');
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs.engine({
+    extname: '.hbs',
+    defaultLayout: 'layout.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+socketServer.on('connection', (client) => {
+    console.log("Usuario conectado:", client.id);
+});
+
+const PORT = process.env.PORT || 8080;
+httpServer.listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`));
