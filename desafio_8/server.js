119e3a0cb8058c5323beb21bd0b85e78211b1f2d
Martin Guevara <martin_91@live.com.ar>
2022-10-11 18:57:09 -0300

    Desafio: Nuestra primera base de datos

--- desafio_8/server.js
+++ desafio_8/server.js
@@ -1,4 +1,6 @@
+import http from 'http';
 import express from 'express';
+import { Server as SocketServer } from 'socket.io';
 import productsRoutes from './routes/productsRoutes.js'
 import messagesRoutes from './routes/messagesRoutes.js'
 
@@ -9,5 +11,12 @@ app.use(express.urlencoded({extended: true}))
 app.use('/products', productsRoutes);
 app.use('/messages', messagesRoutes);
 
+const httpServer = http.createServer(app);
+const socketServer = new SocketServer(httpServer);
+
+socketServer.on('connection', (client) => {
+    console.log("Usuario conectado:", client.id);
+});
+
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`))
+httpServer.listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`));
