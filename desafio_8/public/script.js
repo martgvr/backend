8ab3345d66023d4a4c2401452ff1881ab6e69738
Martin Guevara <martin_91@live.com.ar>
2022-10-12 17:16:56 -0300

    Desafio: Nuestra primera base de datos

--- desafio_8/public/script.js
+++ desafio_8/public/script.js
@@ -1,1 +1,70 @@
-const socket = io('http://localhost:8080');
+const socketClient = io('http://localhost:8080');
+
+const productsTable = document.getElementById('productsTable');
+
+const messagesContainer = document.getElementById('messagesContainer');
+const messagesForm = document.getElementById('messagesForm');
+const messageName = document.getElementById('messageName');
+const messageText = document.getElementById("messageText");
+
+// CONTROL DE FORMULARIOS
+messagesForm.onsubmit = (e) => {
+    e.preventDefault();
+    const name = messageName.value;
+    const message = messageText.value;
+    const timestamp = new Date().toLocaleString();
+
+    socketClient.emit("newMessage", { name, message, timestamp });
+    messageText.value = "";
+};
+
+// EVENTOS PROVENIENTES DEL SERVIDOR
+socketClient
+    .on("loadMessages", (messages) => {
+        loadMessages(messages);
+        scrollBottom();
+    })
+
+    .on("loadProducts", (products) => {
+        loadProducts(products);
+    });
+
+    
+// FUNCIONES
+function sendMessage(event) {
+    event.preventDefault();
+}
+
+function loadMessages(messages) {
+    const inner = messages.map((message) => {
+        return `    <li>
+                        <div class="mensajeContainer">
+                            <div class="nick">
+                                <p style="color: blue">${message.name}</p><p style="color: brown">[${message.timestamp}]:</p><p style="color: green">${message.message}</p>
+                            </div>
+                        </div>
+                    </li>`;
+
+    }).join(" ");
+    messagesContainer.innerHTML = inner;
+}
+
+function scrollBottom() {
+    messagesContainer.scroll({
+        top: messagesContainer.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+
+function loadProducts(products) {
+    const inner = products.map((item) => {
+        return `
+                <tr>
+                    <td>${item.title}</td>
+                    <td>${item.price}</td>
+                    <td><img src="${item.thumbnail}" /></td>
+                </tr>
+                `;
+    }).join(" ");
+    productsTable.innerHTML = inner;
+}
--- desafio_8/server.js
+++ desafio_8/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import productsRoutes from './routes/productsRoutes.js'
 import messagesRoutes from './routes/messagesRoutes.js'
 
+import { messages_Config as messagesDB } from './db/dbConfig.js'
+import { products_Config as productsDB } from './db/dbConfig.js'
 import http from 'http';
 import { Server } from "socket.io";
 
@@ -17,6 +19,16 @@ const socketServer = new Server(httpServer, { cors: { origin: "*" }});
 
 socketServer.on('connection', (client) => {
     console.log("Usuario conectado:", client.id);
+
+    messagesDB.getAll().then(messages => socketServer.sockets.emit("loadMessages", messages));
+    productsDB.getAll().then(products => socketServer.sockets.emit("loadProducts", products));
+
+    // CLIENTE EMITE
+    client.on("newMessage", (data) => {
+        console.log(data);
+        messagesDB.save(data);
+        messagesDB.getAll().then(messages => socketServer.sockets.emit("loadMessages", messages));
+    });
 });
 
 const PORT = process.env.PORT || 8080;
