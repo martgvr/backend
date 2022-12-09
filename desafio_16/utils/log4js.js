fec82154b0361b1cc103c51b71330be92402a1d2
Martin Guevara <martin_91@live.com.ar>
2022-12-09 19:10:39 -0300

    Desafio: Loggers, gzip y analisis de performance

--- desafio_16/utils/log4js.js
+++ desafio_16/utils/log4js.js
@@ -2,21 +2,19 @@ import log4js from 'log4js'
 
 log4js.configure({
     appenders: {
-        myConsole: { type: 'console' },
-        myFile: { type: 'file', filename: 'logs.log' },
+        console: { type: 'console' },
+        file: { type: 'file', filename: 'logs.log' },
 
         // Appenders compuestos
-        logConsole: { type: 'logLevelFilter', appender: 'myConsole', level: 'trace' },
-        logFile: { type: 'logLevelFilter', appender: 'myFile', level: 'trace' }
+        logConsole: { type: 'logLevelFilter', appender: 'console', level: 'trace' },
+        logFile: { type: 'logLevelFilter', appender: 'file', level: 'trace' }
     },
 
     categories: {
-        default: { appenders: ['myConsole'], level: 'trace' },
-
-        console: { appenders: ['myConsole'], level: 'trace' },
-        file: { appenders: ['myFile'], level: 'trace' },
-
-        prod: { appenders: ['logConsole', 'logFile'], level: 'all' }
+        default: { appenders: ['console'], level: 'trace' },
+        prod: { appenders: ['logConsole', 'logFile'], level: 'all' },
+        // console: { appenders: ['console'], level: 'trace' },
+        // file: { appenders: ['file'], level: 'trace' }
     }
 })
 
