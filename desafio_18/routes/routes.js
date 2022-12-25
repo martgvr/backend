3d3c94639b5f5c31072178b759c582d6b433b676
Martin Guevara <martin_91@live.com.ar>
2022-12-25 10:33:07 -0300

    Desafio: Tercera entrega del Proyecto Final

--- desafio_18/routes/routes.js
+++ desafio_18/routes/routes.js
@@ -1,4 +1,3 @@
-import os from 'os'
 import passport from "passport";
 import { Router } from "express";
 import { isAuth } from '../middleware/isAuth.js'
@@ -31,10 +30,9 @@ router.post('/login', passport.authenticate('login', {
 router.get('/logerror', (req, res) => res.render('error', { text: 'Usuario o contraseña incorrectos.', type: 'logerror' }))
 router.get('/regerror', (req, res) => res.render('error', { text: 'Este usuario ya se encuentra registrado.', type: 'regerror' }))
 
-router.get('/data', isAuth, (req, res) => res.render('data', { username: req.user.username }))
+router.get('/profile', isAuth, (req, res) => res.render('profile', { username: req.user.username }))
 
 router.get('/products', isAuth, (req, res) => {
-    console.log('Cargando productos...');
     db.getAll().then(data => res.render('products', { username: req.user.username, data: data }))
 })
 
@@ -43,18 +41,5 @@ router.get('/logout', (req, res) => {
         res.redirect('/')
     })
 })
-
-const processData = {
-    args: process.argv.slice(2),
-    os: process.platform,
-    nodeVersion: process.versions.node,
-    rss: process.memoryUsage.rss(),
-    execPath: process.execPath,
-    pid: process.pid,
-    path: process.cwd(),
-    cpus: os.cpus().length
-}
-
-router.get('/info', (req, res) => res.render('info', { processData }))
 
 export default router
