aed20e96a4d99a29357301ab4cb3964de0e16b70
Martin Guevara <martin_91@live.com.ar>
2023-01-16 17:17:00 -0300

    Desafio: Dividir en capas nuestro proyecto

--- desafio_19/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import productsController from '../controllers/products.controller.js'
-
-const router = express.Router()
-
-router.get('/', isAuth, productsController.productsRedirect)
-router.get('/products', isAuth, productsController.getProducts)
-
-export default router
