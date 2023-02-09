5a9feec0f9e43b7a8184d1f9ae2de67b68ae79e3
Martin Guevara <martin_91@live.com.ar>
2023-02-08 22:04:02 -0300

    Desafio: Reformar para usar otro framework

--- desafio_23/src/app.module.ts
+++ desafio_23/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { CartsModule } from './carts/carts.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [CartsModule, ProductsModule, UsersModule],
+  
+  imports: [CartsModule, ProductsModule, UsersModule, MongooseModule.forRoot('mongodb+srv://root:rootmongo123456@cluster0.vpzccsu.mongodb.net/users?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
