1c0dd01b84a97d1355f6efb7c5bc13288e9625bc
Martin Guevara <martin_91@live.com.ar>
2023-02-13 19:10:48 -0300

    Desafio: Servidor en Deno

--- desafio_24/src/server.ts
+++ desafio_24/src/server.ts
@@ -3,9 +3,21 @@ import { Application } from "oak"
 import { logger } from "@utils/loggers.ts"
 import apiRoutes from '@routes/index.routes.ts'
 
+import { viewEngine, handlebarsEngine, oakAdapter } from "https://deno.land/x/view_engine@v10.5.1c/mod.ts"
+
 const { PORT = 3000 } = config()
 
 const app = new Application()
+
+app.use(
+    viewEngine(oakAdapter, handlebarsEngine, {
+      viewRoot: "./views/",
+    })
+  );
+
+app.use((ctx) => {
+    ctx.render("index.hbs", { colors: "Rojo" } );
+});
 
 app.use(apiRoutes.routes())
 
