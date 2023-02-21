15cc1a8c69c3afc131b71d5346c064c0ec950487
Martin Guevara <martin_91@live.com.ar>
2023-02-21 15:38:22 -0300

    Proyecto final

---
+++ proyecto_final/server/views/scripts/chat.js
@@ -0,0 +1,23 @@
+async function postMessage(event, name, username) {
+    event.preventDefault()
+
+    const chatInputValue = document.getElementById('chatInput')
+    const currentTime = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' })
+    console.log(currentTime);
+
+    dataToSave = { name: name, username: username, text: chatInputValue.value, timestamp: currentTime }
+
+    if (chatInputValue !== '') {
+        chatInputValue.value = ''
+
+        const data = await fetch(`http://localhost:8080/chat`, 
+        { 
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataToSave)
+        })
+        .then(response => response.json())
+    
+        data.message === 'Query successfully resolved' && window.location.reload()
+    }
+}
